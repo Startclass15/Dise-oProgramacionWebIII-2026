6bd0142bb3bde50843ad7aceb5d1d183cc681980
Jack625a <ktfut15@gmail.com>
2026-04-09 20:02:19 -0400

    Alertas pantallas emergentes EN primer plano

--- primero/src/app/contenido/contenido.ts
+++ primero/src/app/contenido/contenido.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import {MatDialogModule,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+
+
+} from '@angular/material/dialog';
+import { Contenido3 } from '../contenido3/contenido3';
+import {MatButtonModule} from '@angular/material/button'
+import { Contenido2 } from '../contenido2/contenido2';
+import { Contenido4 } from '../contenido4/contenido4';
 
 @Component({
   selector: 'app-contenido',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './contenido.html',
   styleUrl: './contenido.css',
 })
-export class Contenido {}
+export class Contenido {
+  readonly alerta=inject(MatDialog)
+  mostrarAlerta():void{
+    const alertaRef=this.alerta.open(Contenido2)
+  }
+}
